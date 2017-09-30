9216c07f61652712d217942412b93126989e1d5d
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2017-09-30 06:03:49 +0330

    custom serializer

--- bin/run-client.sh
+++ bin/run-client.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp target/nosql-0.1.jar ir.infra.clients.RandomEmsInfoWriter localhost cassandra/add/emsInfo 10 5 3600
+java -cp target/nosql-0.1.jar ir.infra.clients.RandomEmsInfoWriter localhost cassandra/add/emsInfo 1 5 30
