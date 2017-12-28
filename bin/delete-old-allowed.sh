8da6233884699815463e6563d2dc8d0cca269026
Mehdi Alemi <mehdi.alemi.se@gmail.com>
2017-12-29 01:16:35 +0330

    delete old rows than 2 month and  allowed = true

--- bin/delete-old-allowed.sh
+++ bin/delete-old-allowed.sh
@@ -2,4 +2,4 @@
 
 old_allowed_sec=$((2*31*86400))
 echo "Delete invalid rows older than $old_allowed_sec seconds with allowed = true field"
-java -cp target/nosql-0.1.jar ir.infra.cassandra.DeleteOldAllowed $old_allowed_sec
+java -cp target/nosql-0.1.jar ir.infra.cassandra.DeleteOldAllowed $HOSTNAME $old_allowed_sec
--- src/main/java/ir/infra/cassandra/DeleteOldAllowed.java
+++ src/main/java/ir/infra/cassandra/DeleteOldAllowed.java
@@ -5,8 +5,10 @@ import ir.infra.core.ClusterConf;
 public class DeleteOldAllowed {
 
     public static void main(String[] args) throws Exception {
-        int old_allowed_sec = Integer.parseInt(args[0]);
+        String hostname = args[0];
+        int old_allowed_sec = Integer.parseInt(args[1]);
         ClusterConf conf = new ClusterConf();
+        conf.setCoordinator(hostname);
         conf.setOld_allowed_sec(old_allowed_sec);
         conf.setEnable_test(false);
         CassandraClient cassandraClient = new CassandraClient(conf);
